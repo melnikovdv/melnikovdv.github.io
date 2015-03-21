eee4c9322da01353ac5a3c45715585af92531d19
mdv <melnikovdv@gmail.com>
2015-03-21 22:44:28 +0300

    random dev joke added

--- assets/js/my.js
+++ assets/js/my.js
@@ -1,8 +1,12 @@
-/*
- * some hint to avoid buzzing in templates
- */
+function onLoad() {    
+  setupHeader();
+  showJoke();
+};
 
-(window.onload = function () {  
+(window.onload = onLoad);
+
+// some hint to avoid buzzing in templates
+function setupHeader() {
   var active = "blog-nav-item active";
   var usual = active.split(" ")[0];
   
@@ -18,4 +22,26 @@
   } else if ((document.URL == location.origin + "/") || (document.URL == location.origin + "/index.html")){
     $home.className += " active"
   }  
-});
+};
+
+function showJoke() {    
+  var s = $.getJSON("/assets/data/jokes.json", function(jokes) {
+    i = Math.floor(Math.random() * jokes.dev.length);
+
+    var div = document.createElement("div");
+    div.className = "sidebar-module sidebar-module-inset";
+    
+    var p = document.createElement("h4");
+    p.innerText = "Dev humour";
+    div.appendChild(p);
+
+    p = document.createElement("p");    
+    p.innerText = jokes.dev[i];
+    div.appendChild(p);    
+
+    document.getElementById("sidebar").appendChild(div);    
+  });
+  
+};
+
+
